2c1376bf47eb57f7c8786e54a9862eaa94344fa2
barraotieno <barrackotieno160@gmail.com>
2025-05-04 17:08:31 +0300

    add: eventlisteners.

---
+++ Drum-Kit/index.js
@@ -0,0 +1,7 @@
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("i got clicked");
+    });
+}
